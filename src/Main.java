f217b36da06bec77a79e9518b5c568df81cee73e
yrai3646 <yrai3646@zenithacademy.org>
2026-02-25 14:41:10 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ public class Main {
 
 
 
-        double Homemaintancecost = 0;
-        double Season1 = 0;
-        double Season2 = 0;
-        double Season3 = 0;
-        double Season4 = 0;
-        double Totalyearlymaintancecost = 0;
+        double Homemaintancecost = 2;
+        double Season1 = 25;
+        double Season2 = 22;
+        double Season3 = 29;
+        double Season4 = 20;
+        double Totalyearlymaintancecost;
+
+        Totalyearlymaintancecost = Season1 + Season2 + Season3 + Season4;
 
 
-        System.out.println(" Total Yearly Maintance Cost Is " + Season1 + Season2 + Season3 + Season4);
+        System.out.println(" Total Yearly Maintance Cost Is " + Totalyearlymaintancecost);
     }
 }
