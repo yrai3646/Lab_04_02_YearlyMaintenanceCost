ab0ae227729ff55d1ad34663813c47286f1f979c
yrai3646 <yrai3646@zenithacademy.org>
2026-02-25 14:30:44 -0500

    Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        double Homemaintancecost = 0;
+        double Season1 = 0;
+        double Season2 = 0;
+        double Season3 = 0;
+        double Season4 = 0;
+        double Totalyearlymaintancecost = 0;
+
+
+        System.out.println(" Total Yearly Maintance Cost Is " + Season1 + Season2 + Season3 + Season4);
+    }
+}
